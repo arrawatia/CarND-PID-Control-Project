6f8b551ae4b3cfa355a06b42d92e3bc5262392c0
Sumit Arrawatia <sumit.arrawatia@gmail.com>
2019-07-19 22:05:43 -0700

    Ready for submission.

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,7 +12,9 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
-
+  this->Kp = Kp_;
+  this->Kd = Kd_;
+  this->Ki = Ki_;
 }
 
 void PID::UpdateError(double cte) {
@@ -20,11 +22,24 @@ void PID::UpdateError(double cte) {
    * TODO: Update PID errors based on cte.
    */
 
+
+  // Derivative error (p_error is set to the last recorded cte)
+  d_error = cte - p_error;
+
+  // Proportional error 
+  p_error = cte;
+
+  // Integral error
+  i_error += cte; 
+
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  return (-Kp * p_error) + // Proportional term
+         (-Kd * d_error) + // Derivative term
+         (-Ki * i_error)   // Integral term
+         ;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,19 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  // Proportional
+  // pid.Init(1, 0.0, 0.0);
+
+  // Integral
+  // pid.Init(0.0, 1.0, 0.0);
+
+  // Derivative
+  // pid.Init(0.0, 0.0, 1.0);
+
+  // PID
+  pid.Init(0.15, 0.00031, 2.5);
+
+
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -64,6 +77,10 @@ int main() {
            *   Maybe use another PID controller to control the speed!
            */
           
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+          
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
